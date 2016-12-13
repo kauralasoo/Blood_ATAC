8151a71845438a09a66f07cf5807065e2db6511e
Kaur Alasoo <kaur.alasoo@gmail.com>
2016-12-13 13:45:56 +0000

    Make a separate folder for download scripts

---
+++ download_scripts/neutrophils.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+wget https://sra-download.ncbi.nlm.nih.gov/srapub/SRR2999312
+wget https://sra-download.ncbi.nlm.nih.gov/srapub/SRR2999313
+wget https://sra-download.ncbi.nlm.nih.gov/srapub/SRR2999314
+wget https://sra-download.ncbi.nlm.nih.gov/srapub/SRR2999315
+wget https://sra-download.ncbi.nlm.nih.gov/srapub/SRR2999316
+wget https://sra-download.ncbi.nlm.nih.gov/srapub/SRR2999317
+wget https://sra-download.ncbi.nlm.nih.gov/srapub/SRR2999318
+wget https://sra-download.ncbi.nlm.nih.gov/srapub/SRR2999319
+wget https://sra-download.ncbi.nlm.nih.gov/srapub/SRR2999320
+wget https://sra-download.ncbi.nlm.nih.gov/srapub/SRR2999321
+wget https://sra-download.ncbi.nlm.nih.gov/srapub/SRR2999322
+wget https://sra-download.ncbi.nlm.nih.gov/srapub/SRR2999323
+wget https://sra-download.ncbi.nlm.nih.gov/srapub/SRR2999324
+wget https://sra-download.ncbi.nlm.nih.gov/srapub/SRR2999325
+wget https://sra-download.ncbi.nlm.nih.gov/srapub/SRR2999326
+wget https://sra-download.ncbi.nlm.nih.gov/srapub/SRR2999327
+wget https://sra-download.ncbi.nlm.nih.gov/srapub/SRR2999328
+wget https://sra-download.ncbi.nlm.nih.gov/srapub/SRR2999329
+wget https://sra-download.ncbi.nlm.nih.gov/srapub/SRR2999330
+wget https://sra-download.ncbi.nlm.nih.gov/srapub/SRR2999331
+wget https://sra-download.ncbi.nlm.nih.gov/srapub/SRR2999332
+wget https://sra-download.ncbi.nlm.nih.gov/srapub/SRR2999333
