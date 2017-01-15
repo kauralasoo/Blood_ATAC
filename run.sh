07ddeb68bb01fc266403edb0684ca13c2d41d0c9
Kaur Alasoo <kaur.alasoo@gmail.com>
2017-01-15 13:18:56 +0000

    Add commands to execute Snakemake

---
+++ run.sh
@@ -0,0 +1,11 @@
+#Process Blood ATAC data
+#Dry run
+snakemake -np processed/out.txt
+#Cluster run
+snakemake --cluster scripts/snakemake_submit.py -p processed/out.txt --jobs 100
+
+#Process Macrophage ATAC data
+#Dry run
+snakemake -np macrophages/out.txt
+#Cluster run
+snakemake --cluster scripts/snakemake_submit.py -p macrophages/out.txt --jobs 100 —configfile config_macrophages.yaml
